b4121e551aa6b8bc460dc1d74f29862886d1e9e1
Kevin McQuown <kevinmcquown@Kevins-MacBook.local>
2015-09-13 09:08:43 -0500

    gets value properly from peripheral

--- IDBeacon.swift
+++ IDBeacon.swift
@@ -9,27 +9,33 @@
 import CoreBluetooth
 
 let customServiceUUID = CBUUID(string: "FA0F2A39-84FE-4C27-9C83-B73B060E3DA0")
-let primaryService : CBMutableService! = CBMutableService(type: customServiceUUID, primary: true);
-var advertisementPayload = [CBAdvertisementDataServiceUUIDsKey : [primaryService.UUID],
-    CBAdvertisementDataLocalNameKey: "GetDibbsPhone",
-    CBAdvertisementDataIsConnectable: true
-];
+let userIDCharacteristicUUID = CBUUID(string: "F5A1056C-CD3D-46D4-BBBE-F1124F06CD42")
 
 class IDBeacon : NSObject, CBPeripheralManagerDelegate {
 
     static let sharedInstance = IDBeacon();
     
+    var advertisementPayload : [String : AnyObject]!
+    var data : NSData!
+    var primaryService : CBMutableService!
+    var userIDCharacteristic : CBMutableCharacteristic!
+    
     var manager : CBPeripheralManager!
     
     @objc func peripheralManager(peripheral: CBPeripheralManager, didAddService service: CBService, error: NSError?) {
-        if error == nil
+        if let err = error
         {
-            print("service added successfully");
+            print(err.localizedDescription);
         }
         else
         {
-            print("error adding service");
+            print("service added successfully");
+            manager.startAdvertising(advertisementPayload);
         }
+    }
+    
+    func peripheralManager(peripheral: CBPeripheralManager, didReceiveReadRequest request: CBATTRequest) {
+        print("read request received");
     }
     
     func peripheralManagerDidUpdateState(peripheral: CBPeripheralManager) {
@@ -39,8 +45,19 @@ class IDBeacon : NSObject, CBPeripheralManagerDelegate {
                 
                 print("Peripheral Manager Powered on");
                 
+                primaryService = CBMutableService(type: customServiceUUID, primary: true);
+                
+                userIDCharacteristic  = CBMutableCharacteristic(type: userIDCharacteristicUUID, properties: CBCharacteristicProperties.Read, value: data, permissions: CBAttributePermissions.Readable);
+                
+                print("created characteristic with uuid \(userIDCharacteristic.UUID.UUIDString)")
+                primaryService.characteristics = [userIDCharacteristic];
+
+                advertisementPayload = [CBAdvertisementDataServiceUUIDsKey : [primaryService.UUID],
+                    CBAdvertisementDataLocalNameKey: "GetDibbsPhone",
+                    CBAdvertisementDataIsConnectable: true
+                ];
+
                 manager.addService(primaryService);
-                manager.startAdvertising(advertisementPayload);
 
             default: break
         }
@@ -56,6 +73,7 @@ class IDBeacon : NSObject, CBPeripheralManagerDelegate {
         manager.stopAdvertising();
     }
     
+    
     func peripheralManagerDidStartAdvertising(peripheral: CBPeripheralManager, error: NSError?) {
         if error == nil{
             print("Did start advertising");
@@ -63,10 +81,9 @@ class IDBeacon : NSObject, CBPeripheralManagerDelegate {
     }
     
     func start(companyID : UInt16) {
-        
         var value = companyID;
-        let data = NSData(bytes: &value, length: 2);
-        advertisementPayload[CBAdvertisementDataManufacturerDataKey] = data;
+        data = NSData(bytes: &value, length: 2);
+
         manager = CBPeripheralManager(delegate: self, queue: nil);
         
     }
